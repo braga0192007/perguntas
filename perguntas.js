4df9a625c39f947558df4fc7796edf4566d70d4e
braga0192007 <guibragaandrade2007@gmail.com>
2024-11-14 13:43:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual a historia do corinthians?',
+    'Em 1º de setembro de 1910 um grupo de operários do bairro do Bom Retiro, localizado em São Paulo, inspirados na passagem de um time Inglês pelo Brasil, denominado Corinthian Football Club, criaram o Sport Club Corinthians Paulista.'
+)
+
+criaCartao(
+    'Games',
+    'Qual o ultimo lançamento da activision',
+    'O ultimo lançamento da actvision foi o Call Of Duty: Black Ops 6, lançado dia 25/10/2024'
+)
+
+criaCartao(
+    'Futebol',
+    'Quantas bolas de ouro o Cristiano Ronaldo tem?',
+    'O Cristiano Ronaldo tem 5 bolas de ouro'
+)
+
+criaCartao(
+    'Games',
+    'Quais são os consoles de nova geração?',
+    'Os consoles de nova geração são os Xbox Series S, Xbos Series X e PlayStation 5'
+)
